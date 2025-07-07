a0a5dc40cf89430793f252cdffe675a52e07b946
sanchayanroy <71485684+sanchayanroy@users.noreply.github.com>
2025-07-07 22:57:59 +0530

    Update HS_DSA_Ex-2.2_prob_6.c

--- HS_DSA_Ex-2.2_prob_6.c
+++ HS_DSA_Ex-2.2_prob_6.c
@@ -12,10 +12,10 @@ exit(EXIT_FAILURE);\
 
 void transpose(int **a, int **b, int rows, int cols)
 {
-	MALLOC(b,cols*sizeof(*x));
+	MALLOC(b,cols*sizeof(*b));
 	int i,j;
 	for(i=0;i<cols;i++)
-		CALLOC(b[i],rows,sizeof(**x));
+		CALLOC(b[i],rows,sizeof(**b));
 	for(i=0;i<rows;i++)
 		for(j=0;j<cols;j++)
 			b[j][i]=a[i][j];
